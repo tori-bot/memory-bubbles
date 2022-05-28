f974f30c0d831da2d788daf555946666e5556aad
victoria makena <makenavictoria1@gmail.com>
2022-05-28 14:22:00 +0300

    templates: configure urls

--- p_gallery/views.py
+++ p_gallery/views.py
@@ -1,6 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+# def home(request):
+#     return HttpResponse("hello world")
 def home(request):
     welcome="hello. welcome to Makena's memory bubbles."
-    return render('index.html', {'welcome':welcome})
+    return render(request,'index.html', {'welcome':welcome})
--- photos/settings.py
+++ photos/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'bootstrap5',
+    'p_gallery',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
