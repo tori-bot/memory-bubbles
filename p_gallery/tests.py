c01da9ba1fe4f29ce977c9f38c36d3332623c0e7
victoria makena <makenavictoria1@gmail.com>
2022-05-28 15:59:15 +0300

    db:add tests for model methods

--- p_gallery/tests.py
+++ p_gallery/tests.py
@@ -1,3 +1,31 @@
+
 from django.test import TestCase
+from .models import Image,Category,Location
 
 # Create your tests here.
+class ImageTestCase(TestCase):
+    
+    def setUp(self):
+        self.image =Image(image_name='image',image_description='image description',image_location=self.location,image_category=self.category)
+        self.image.save_image()
+
+
+        self.category = Category(category='category')
+        self.category.save()
+
+        self.location = Location(location='location')
+        self.location.save()
+
+    def tearDown(self):
+        Image.objects.all().delete()
+        Category.objects.all().delete()
+        Location.objects.all().delete()
+    
+    def test_get_imageby_id(self):
+        image=Image.get_image_by_id()
+        self.assertTrue(len(image)>0)
+
+    def test_search_image(self):
+        term='school'
+        results=Image.search_image(term)
+        self.assertTrue(len(results)==0)
