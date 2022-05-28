45c43f6d4b629392478ef722b20eba52b68bf487
victoria makena <makenavictoria1@gmail.com>
2022-05-28 12:25:40 +0300

    init: define home view

--- p_gallery/views.py
+++ p_gallery/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    welcome="hello. welcome to Makena's memory bubbles."
+    return render('index.html', {'welcome':welcome})
