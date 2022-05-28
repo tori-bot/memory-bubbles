ba9561f66f2f7920baa3a7f767fad657abdaee9d
victoria makena <makenavictoria1@gmail.com>
2022-05-28 15:35:28 +0300

    db:add query methods

--- p_gallery/models.py
+++ p_gallery/models.py
@@ -1,3 +1,36 @@
 from django.db import models
-
+import datetime as dt
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to='photo/',null=True)
+    image_name=models.CharField(max_length=20)
+    image_description=models.TextField()
+    image_location=models.ForeignKey('Location',on_delete=models.DO_NOTHING,null=True)
+    image_category=models.ForeignKey('Category',on_delete=models.DO_NOTHING,null=True)
+    published=models.DateTimeField(auto_now_add=True)
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def update_image(self):
+        self.save()
+
+    @classmethod
+    def get_image_by_id(cls,id):
+        image=cls.objects.get(id=id)
+        return image
+
+    @classmethod
+    def search_image(cls,category):
+        image=cls.objects.filter(category__icontains=category) 
+        return image
+
+
+class Location(models.Model):
+    place= models.CharField(max_length=30,null=True)
+
+class Category(models.Model):
+    category=models.CharField(max_length=20,null=True)
