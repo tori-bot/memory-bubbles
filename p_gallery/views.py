54f410b8e55aa5e2e4e1a99ad12e9d22ab4930e4
victoria makena <makenavictoria1@gmail.com>
2022-05-29 23:08:28 +0300

    views:add search view function

--- p_gallery/views.py
+++ p_gallery/views.py
@@ -1,9 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Image
 # Create your views here.
 # def home(request):
 #     return HttpResponse("hello world")
 def home(request):
     welcome="hello. welcome to Makena's memory bubbles."
     return render(request,'index.html', {'welcome':welcome})
+
+def search(request):
+    if 'image' in request.GET and request.GET['image']:
+        #check if the article query exists in our request.GET object and then we then check if it has a value
+        category = request.GET.get('category')
+        searched_images=Image.search_image(category)
+        message=f'{category} '
+        return render(request,'search.html',{'searched_images':searched_images,'message':message})
+    else:
+        message='Try searching for something'
+        return render(request,'search.html',{'message':message})
